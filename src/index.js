5a414202acceb42b4384053f3ddf26a9418212bb
rbbri <robin.s.johnson@gmail.com>
2019-01-21 11:37:03 +0000

    Project setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import World from './containers/World'
+import './index.css';
+
+ReactDOM.render(<World />, document.getElementById('world'));
